cb38a3c58c82bd9e014ffdd153db15ec6da1de7a
Roanldas Macas <ronaldas.macas@port.ac.uk>
2023-10-29 11:59:08 +0000

    added option to create a folder if not exists

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def chdir(dir_, logger):
+def chdir(dir_, logger, create=False):
     if not dir_.is_dir():
-        logger.error(f"The directory {dir_} doesn't exist")
-        raise SystemExit(1)
+        if create:
+            logger.warning(f"The directory {dir_} doesn't exist. Creating it")
+            dir_.mkdir(parents=True, exist_ok=True)
+        else:
+            logger.error(f"The directory {dir_} doesn't exist")
+            raise SystemExit(1)
