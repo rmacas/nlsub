a1972240b0b02d116378c2ad16ca58a44c7f1017
Roanldas Macas <ronaldas.macas@port.ac.uk>
2024-01-08 08:40:12 +0200

    moving functions to utils.py

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 import argparse
 import logging
-from src import utils
 import numpy as np
-from gwpy.timeseries import TimeSeries
-from scipy.stats import zscore
 from pathlib import Path
+from src import utils
 
 
 parser = argparse.ArgumentParser()
@@ -16,60 +14,6 @@ parser.add_argument("--outdir",
                     help="output directory, default: data/processed",
                     default=Path('data/processed'))
 args = parser.parse_args()
-
-
-def load_data(indir, channels, fs):
-
-    dset = []
-    norm = []
-    for channel in channels:
-
-        fname = f'{indir}/{channel}'
-        tseries = TimeSeries.read(fname)
-
-        # remove sides due to whitening artifacts
-        data = tseries[fs*4:-fs*4].reshape(-1, 1)
-
-        # normalize and record the normalisation factor
-        norm_factor = np.max(np.abs(data))
-        norm.append(norm_factor)
-        dset.append(data / norm_factor)
-
-    dset = np.squeeze(dset).astype('float32')
-
-    return dset, norm
-
-
-def find_noisy(data, threshold, padding):
-    """ Find idx passing the Z-score threshold."""
-
-    noise_idx = np.argwhere(np.abs(zscore(data)) > threshold).T.tolist()[0]
-
-    # source https://stackoverflow.com/questions/53177358/removing-numbers-which-are-close-to-each-other-in-a-list
-    usedValues = set()
-    noise_idx_isolated = []
-    for v in noise_idx:
-        if v not in usedValues:
-            noise_idx_isolated.append(v)
-            for lv in range(v - padding, v + padding + 1):
-                usedValues.add(lv)
-
-    return noise_idx_isolated
-
-
-def get_features(dset, input, output, idx, padding, size_input, size_future):
-    """ Build input and output arrays based on the network architecture."""
-    size_past = size_input - size_future
-    box_start = idx - padding
-    box_end = idx + padding
-    for i in range(box_start+size_past, box_end-size_future):
-        array = np.array([dset[1, i-size_past:i+size_future],
-                          dset[2, i-size_past:i+size_future],
-                          dset[3, i-size_past:i+size_future]])
-        input.append(array)
-    output.append(dset[0, box_start+size_past:box_end-size_future])
-
-    return input, output
 
 
 def main(indir, outdir):
@@ -92,14 +36,14 @@ def main(indir, outdir):
                 'L1:LSC-POP_A_RF9_I_ERR_DQ']
     channels = [f'{channel[3:]}_{suffix}' for channel in channels]
 
-    dset, norm = load_data(indir, channels, fs)
+    dset, norm = utils.load_data(indir, channels, fs)
 
     logger.info('Processing data')
     logger.warning('This is a memory-intensive task. Requires O(20GB) '
                    'memory')
     padding = fs * 4  # amount of data considered 'related' around an idx
     threshold = 11.7  # found by trial for dset[1]
-    noisy_idx = find_noisy(dset[1], threshold, padding)
+    noisy_idx = utils.find_noisy(dset[1], threshold, padding)
 
     # input array params
     size_input = int(fs*1.5)
@@ -111,14 +55,14 @@ def main(indir, outdir):
     input = []
     output = []
     for idx in noisy_idx:
-        input, output = get_features(dset, input, output, idx, padding,
-                                     size_input, size_future)
+        input, output = utils.get_features(dset, input, output, idx, padding,
+                                           size_input, size_future)
     input = np.array(input)
     output = np.hstack(output).reshape(-1, 1)
 
     # remove glitches
     glitch_threshold = 40  # threshold factor found by trial
-    glitch_idx = find_noisy(output, glitch_threshold, padding)
+    glitch_idx = utils.find_noisy(output, glitch_threshold, padding)
 
     for idx in glitch_idx[::-1]:
         idx_start = idx - padding - size_input - size_future
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import numpy as np
+from gwpy.timeseries import TimeSeries
+from scipy.stats import zscore
+
 
 def chdir(dir_, logger, create=False):
     if not dir_.is_dir():
@@ -9,3 +13,57 @@ def chdir(dir_, logger, create=False):
         else:
             logger.error(f"The directory {dir_} doesn't exist")
             raise SystemExit(1)
+
+
+def load_data(indir, channels, fs):
+
+    dset = []
+    norm = []
+    for channel in channels:
+
+        fname = f'{indir}/{channel}'
+        tseries = TimeSeries.read(fname)
+
+        # remove sides due to whitening artifacts
+        data = tseries[fs*4:-fs*4].reshape(-1, 1)
+
+        # normalize and record the normalisation factor
+        norm_factor = np.max(np.abs(data))
+        norm.append(norm_factor)
+        dset.append(data / norm_factor)
+
+    dset = np.squeeze(dset).astype('float32')
+
+    return dset, norm
+
+
+def find_noisy(data, threshold, padding):
+    """ Find idx passing the Z-score threshold."""
+
+    noise_idx = np.argwhere(np.abs(zscore(data)) > threshold).T.tolist()[0]
+
+    # source https://stackoverflow.com/questions/53177358/removing-numbers-which-are-close-to-each-other-in-a-list
+    usedValues = set()
+    noise_idx_isolated = []
+    for v in noise_idx:
+        if v not in usedValues:
+            noise_idx_isolated.append(v)
+            for lv in range(v - padding, v + padding + 1):
+                usedValues.add(lv)
+
+    return noise_idx_isolated
+
+
+def get_features(dset, input, output, idx, padding, size_input, size_future):
+    """ Build input and output arrays based on the network architecture."""
+    size_past = size_input - size_future
+    box_start = idx - padding
+    box_end = idx + padding
+    for i in range(box_start+size_past, box_end-size_future):
+        array = np.array([dset[1, i-size_past:i+size_future],
+                          dset[2, i-size_past:i+size_future],
+                          dset[3, i-size_past:i+size_future]])
+        input.append(array)
+    output.append(dset[0, box_start+size_past:box_end-size_future])
+
+    return input, output
